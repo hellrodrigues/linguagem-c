508e434ebee6bd7dd1b869b274523a6f12c85d3c
Hellen Rodrigues <he.rodriigues@gmail.com>
2025-09-16 16:49:12 -0300

    atualizacao

---
+++ src/array2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system ("clear");
+
+    int valores [7] = {10,56,30,10,1,47,59};
+    int menor = valores[0];
+    for (int i=1 ; i <= 6 ; i++){
+        if(valores[i] < menor){
+            menor=valores[i];
+        }
+    }
+
+    printf("o menor valor é %d\n",menor);
+    return 0;
+}
